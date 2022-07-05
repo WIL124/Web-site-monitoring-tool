edbfd65db5b22f595833c60997553c1add813f48
Владислав Инютин <inyutin2000@mail.ru>
2022-07-05 14:55:58 +0600

    init

---
+++ src/main/java/thumbtack/school/step3/Step3Application.java
@@ -0,0 +1,13 @@
+package thumbtack.school.step3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Step3Application.class, args);
+    }
+
+}
