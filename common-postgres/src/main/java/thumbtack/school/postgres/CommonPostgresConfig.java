b93117256a963613187297231923cf4c3d0e4468
Владислав Инютин <inyutin2000@mail.ru>
2022-11-18 14:50:25 +0600

    CommonPostgresConfig fix

--- common-postgres/src/main/java/thumbtack/school/postgres/CommonPostgresConfig.java
+++ common-postgres/src/main/java/thumbtack/school/postgres/CommonPostgresConfig.java
@@ -12,18 +12,10 @@ import java.io.InputStream;
 @Configuration
 public class CommonPostgresConfig {
     @Bean
-    public DatabaseReader databaseReader(File databaseFile) throws IOException {
-        return new DatabaseReader.Builder(databaseFile).build();
-    }
-
-    @Bean
-    public File databaseFile() {
-        try (InputStream initialStream = this.getClass().getClassLoader().getResourceAsStream("GeoLite2-City.mmdb")) {
-            File target = new File("classpath:db_tmp_file");
-            FileUtils.copyInputStreamToFile(initialStream, target);
-            return target;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public DatabaseReader databaseReader() throws IOException {
+        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("GeoLite2-City.mmdb");
+             DatabaseReader databaseReader = new DatabaseReader.Builder(inputStream).build()) {
+            return databaseReader;
         }
     }
 }
